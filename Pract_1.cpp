1ca0e19b9d3636b31e2ab2d662eff9cbe6e1fa92
Kostiantyn2212 <formazikkosta@gmail.com>
2023-11-06 20:39:44 +0200

    done practical work №6

--- Pract_1.cpp
+++ Pract_1.cpp
@@ -3,6 +3,8 @@
 #include "Student.h"
 #include "Teacher.h"
 #include "Person.h"
+#include "Vector.h"
+#include "Vector.cpp"
 using namespace std;
 int Menu(void)
 {
@@ -17,7 +19,100 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_CTYPE, "ukr");
-    Person* arrayOfPeople[5];
+
+    Vector<int> intVector;
+    for (int i = 0; i < 10; i++)
+    {
+        intVector.push_back(i * 9 + 1);
+    }
+    cout << "Vector цілих чисел: " << endl;
+    for (int i = 0; i < intVector.getSize(); i++)
+    {
+        cout << intVector[i] << " ";
+    }
+    cout << endl;
+    cout << "Vector цілих чисел з видаленим останнім елементом: " << endl;
+    intVector.pop_back();
+    for (int i = 0; i < intVector.getSize(); i++)
+    {
+        cout << intVector[i] << " ";
+    }
+    cout << endl << endl;
+
+    Vector<double> doubleVector;
+    for (int i = 0; i < 8; i++)
+    {
+        doubleVector.push_back(i * 8.72 + 0.01);
+    }
+    cout << "Vector дійсних чисел: " << endl;
+    for (int i = 0; i < doubleVector.getSize(); i++)
+    {
+        cout << doubleVector[i] << " ";
+    }
+    cout << endl;
+    cout << "Vector дійсних чисел з видаленим останнім елементом: " << endl;
+    doubleVector.pop_back();
+    for (int i = 0; i < doubleVector.getSize(); i++)
+    {
+        cout << doubleVector[i] << " ";
+    }
+    cout << endl << endl;
+
+    Vector<string> stringVector;
+    stringVector.push_back("C++");
+    stringVector.push_back("is the best ");
+    stringVector.push_back("programing language!");
+    cout << "Vector рядків: " << endl;
+    for (int i = 0; i < stringVector.getSize(); i++)
+    {
+        cout << stringVector[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Vector рядків з видаленим останнім елементом: " << endl;
+    stringVector.pop_back();
+    for (int i = 0; i < stringVector.getSize(); i++)
+    {
+        cout << stringVector[i] << " ";
+    }
+    cout << endl << endl;
+
+    cout << "Vector об'єктів класів: " << endl;
+    Student student1;
+    Student student2(1, "Чернець", "Василь", "Ігорович", "13.12.2004", "380678541236", "Математичний", 1, "М-158");
+    cout << "Вводимо данні про студента: " << endl;
+    cin >> student1;
+    cout << endl << endl;
+    Teacher teacher1;
+    Teacher teacher2(2, "Зорянов", "Олексій", "Олександрович", "01.01.2001", "38097851369", "ЦК-123", "Історія");
+    cout << "Вводимо данні про вчителя: " << endl;
+    cin >> teacher1;
+    cout << endl << endl;
+    Vector<Student*> studentVector;
+    studentVector.push_back(&student1);
+    studentVector.push_back(&student2);
+    for (int i = 0; i < studentVector.getSize(); i++)
+    {
+        Student* student = studentVector[i];
+        cout << "Student " << i + 1 << ": " << endl;
+        cout << *student;
+        cout << endl;
+    }
+    cout << endl << endl;
+
+    Vector<Teacher*> teacherVector;
+    teacherVector.push_back(&teacher1);
+    teacherVector.push_back(&teacher2);
+    for (int i = 0; i < teacherVector.getSize(); i++)
+    {
+        Teacher* teacher = teacherVector[i];
+        cout << "Teacher " << i + 1 << ": " << endl;
+        cout << *teacher;
+        cout << endl;
+    }
+    cout << endl << endl;
+
+    /*Person* arrayOfPeople[5];
     int rt;
     for (int i = 0; i < 5; i++) {
         rt = Menu();
@@ -35,7 +130,7 @@ int main() {
     for (int i = 0; i < 5; i++) {
         cout << *arrayOfPeople[i];
         arrayOfPeople[i]->printData();
-    }
+    }*/
 
     /*Student student1, student2(1, "Петров", "Іван", "Олександрович", "01.01.2000", "123-45-67", "Історичний", 2, "Група 101"), student3;
     cin >> student1;
---
+++ Vector.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+using namespace std;
+template <typename T>
+class Vector {
+	T* array;
+	int size;
+	int capacity;
+public:
+	Vector();
+	~Vector();
+	int getSize();
+	int getCapacity();
+	void push_back(const T element);
+	void pop_back();
+	T at(int index);
+	T operator[](int index);
+};
